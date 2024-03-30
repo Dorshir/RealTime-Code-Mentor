1a5d34c8b0e31a319970cac84d7f0b9152c5828b
Dor Shir <Dorshir92@gmail.com>
2024-03-30 22:40:50 +0300

    test

--- frontend/my-app/src/components/CodeEditor.jsx
+++ frontend/my-app/src/components/CodeEditor.jsx
@@ -1,22 +1,22 @@
 import React, { useEffect, useState } from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { debounce } from "lodash"; // Import debounce from lodash library
 import "../Styles.css";
 
 const CodeEditor = ({ value, onChange, readOnly }) => {
   const [lines, setLines] = useState([]); // State to store lines of code
 
-  // Update textarea content and lines state when value prop changes
-//   useEffect(() => {
-//     setLines(value.split("\n"));
-//   }, [value]);
-
+  // Debounce the handleChange function
+  const debouncedHandleChange = debounce((newValue) => {
+    onChange(newValue); // Update the value
+  }, 200); // Adjust debounce delay as needed (e.g., 300 milliseconds)
 
   // Function to handle textarea onChange event
   const handleChange = (e) => {
     const newValue = e.target.value;
-    onChange(newValue); // Update the value immediately
-    // setLines(newValue.split("\n"));
+    debouncedHandleChange(newValue); // Call debounced handleChange
+    // setLines(newValue.split("\n")); // You can remove this line if not needed
   };
 
   // Function to render line numbers
@@ -42,7 +42,7 @@ const CodeEditor = ({ value, onChange, readOnly }) => {
           {value}
         </SyntaxHighlighter>
         <textarea
-          onChange={handleChange}
+          onChange={handleChange} // Call debounced handleChange
           className="code-textarea"
           readOnly={readOnly}
           value={value}
