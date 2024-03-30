eef06a4871e4648eb0feae625061538d5319f8c9
Dor Shir <Dorshir92@gmail.com>
2024-03-30 22:21:27 +0300

    test

--- frontend/my-app/src/components/CodeEditor.jsx
+++ frontend/my-app/src/components/CodeEditor.jsx
@@ -1,23 +1,21 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import "../Styles.css";
 
 const CodeEditor = ({ value, onChange, readOnly }) => {
-  const textareaRef = useRef(null); // Ref to textarea element
   const [lines, setLines] = useState([]); // State to store lines of code
 
   // Update textarea content and lines state when value prop changes
-  useEffect(() => {
-    textareaRef.current.value = value;
-    setLines(value.split("\n"));
-  }, [value]);
+//   useEffect(() => {
+//     setLines(value.split("\n"));
+//   }, [value]);
 
   // Function to handle textarea onChange event
   const handleChange = (e) => {
     const newValue = e.target.value;
-    onChange(newValue);
-    setLines(newValue.split("\n"));
+    onChange(newValue); // Update the value immediately
+    // setLines(newValue.split("\n"));
   };
 
   // Function to render line numbers
@@ -43,10 +41,10 @@ const CodeEditor = ({ value, onChange, readOnly }) => {
           {value}
         </SyntaxHighlighter>
         <textarea
-          ref={textareaRef}
           onChange={handleChange}
           className="code-textarea"
           readOnly={readOnly}
+          value={value}
         />
       </div>
     </div>
