963353ca4b8fd4be598cf9bbd100d329ea75c720
Dor Shir <Dorshir92@gmail.com>
2024-03-30 11:54:25 +0300

    Update

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import dotenv from "dotenv";
+import { configureSocket } from "./socket";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT;
+const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN;
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: CLIENT_ORIGIN,
+    methods: ["GET", "POST"],
+  },
+});
+
+configureSocket(io);
+
+httpServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
