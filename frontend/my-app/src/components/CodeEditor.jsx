3268daea301bc8ee10c17108197b4863c1e5314f
Dor Shir <Dorshir92@gmail.com>
2024-03-30 23:32:52 +0300

    Fix

--- frontend/my-app/src/components/CodeEditor.jsx
+++ frontend/my-app/src/components/CodeEditor.jsx
@@ -2,27 +2,35 @@ import React, { useEffect, useState, useRef } from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import "../Styles.css";
+import { debounce } from "lodash";
 
 const CodeEditor = ({ value, onChange, readOnly }) => {
   const [lines, setLines] = useState([]); // State to store lines of code
   const textareaRef = useRef(null); // Ref to textarea element
   const cursorPositionRef = useRef(null); // Ref to store cursor position
-  //  Update textarea content and lines state when value prop changes
+
+  // Debounce the handleChange function
+  const debouncedOnChange = debounce((newValue) => {
+    onChange(newValue);
+  }, 300); // Adjust debounce delay as needed
+
+  // Update textarea content and lines state when value prop changes
   useEffect(() => {
     setLines(value.split("\n"));
   }, [value]);
-  //   Function to handle textarea onChange event with debounce
+
+  // Function to handle textarea onChange event
   const handleChange = (e) => {
     const newValue = e.target.value;
 
     // Save the cursor position
     cursorPositionRef.current = textareaRef.current.selectionStart;
 
-    // Call onChange to update the value
-    onChange(newValue);
-
     // Update the lines state if needed
     setLines(newValue.split("\n"));
+
+    // Call debounced onChange to update the value
+    debouncedOnChange(newValue);
   };
 
   // Function to render line numbers
@@ -58,8 +66,8 @@ const CodeEditor = ({ value, onChange, readOnly }) => {
         </SyntaxHighlighter>
         <textarea
           ref={textareaRef}
-          onChange={handleChange} // Call handleChange
-          onFocus={handleFocus} // Call handleFocus to restore cursor position
+          onChange={handleChange}
+          onFocus={handleFocus}
           className="code-textarea"
           readOnly={readOnly}
           value={value}
