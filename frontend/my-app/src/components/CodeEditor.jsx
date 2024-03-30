e6b8f35b19e6cbbe6617d99e6e614dd89fe8bb85
Dor Shir <Dorshir92@gmail.com>
2024-03-30 23:45:41 +0300

    Fix

--- frontend/my-app/src/components/CodeEditor.jsx
+++ frontend/my-app/src/components/CodeEditor.jsx
@@ -1,20 +1,23 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef, useMemo } from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import "../Styles.css";
-import { debounce } from "lodash";
 
 const CodeEditor = ({ value, onChange, readOnly }) => {
   const [lines, setLines] = useState([]); // State to store lines of code
   const textareaRef = useRef(null); // Ref to textarea element
   const cursorPositionRef = useRef(null); // Ref to store cursor position
 
-  // Debounce the handleChange function
-  const debouncedOnChange = debounce((newValue) => {
-    onChange(newValue);
-  }, 300); 
+  // Memoize the rendered line numbers
+  const renderedLineNumbers = useMemo(() => {
+    return lines.map((_, index) => (
+      <div key={index} className="line-numbers">
+        {index + 1}
+      </div>
+    ));
+  }, [lines]);
 
-  // Update textarea content and lines state when value prop changes
+  // Update textarea content and lines state when value changes
   useEffect(() => {
     setLines(value.split("\n"));
   }, [value]);
@@ -22,29 +25,13 @@ const CodeEditor = ({ value, onChange, readOnly }) => {
   // Function to handle textarea onChange event
   const handleChange = (e) => {
     const newValue = e.target.value;
-
-    // Save the cursor position
     cursorPositionRef.current = textareaRef.current.selectionStart;
-
-    // Update the lines state if needed
+    onChange(newValue);
     setLines(newValue.split("\n"));
-
-    // Call debounced onChange to update the value
-    debouncedOnChange(newValue);
-  };
-
-  // Function to render line numbers
-  const renderLineNumbers = () => {
-    return lines.map((_, index) => (
-      <div key={index} className="line-numbers">
-        {index + 1}
-      </div>
-    ));
   };
 
   // Function to handle textarea focus event
   const handleFocus = () => {
-    // Restore the cursor position if it's saved
     if (cursorPositionRef.current !== null) {
       textareaRef.current.selectionStart = cursorPositionRef.current;
       textareaRef.current.selectionEnd = cursorPositionRef.current;
@@ -55,7 +42,7 @@ const CodeEditor = ({ value, onChange, readOnly }) => {
   return (
     <div className="code-editor-container">
       <span className="background-overlay" />
-      <div className="line-numbers-container">{renderLineNumbers()}</div>
+      <div className="line-numbers-container">{renderedLineNumbers}</div>
       <div className="code-content">
         <SyntaxHighlighter
           language="javascript"
