81776ea98f6a9a33cb687e83ef73232fd14fded6
Dor Shir <Dorshir92@gmail.com>
2024-03-30 12:35:26 +0300

    Test

--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,25 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import cors from "cors";
 import dotenv from "dotenv";
-import { configureSocket } from "./socket.js"
+import { configureSocket } from "./socket.js";
 dotenv.config();
 
 const app = express();
 app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("GET request to the homepage");
+});
 
 const PORT = process.env.PORT;
 const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN;
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-  // cors: {
-  //   origin: CLIENT_ORIGIN,
-  //   methods: ["GET", "POST"],
-  // },
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
 configureSocket(io);
