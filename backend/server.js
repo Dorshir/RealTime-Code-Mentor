b41e220c3961d07d60ff58a49eb2cc0d1d998a86
Dor Shir <Dorshir92@gmail.com>
2024-08-25 19:54:09 +0300

    standard JS

--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,48 @@
-import express from "express";
-import { createServer } from "http";
-import { Server } from "socket.io";
-import cors from "cors";
-import dotenv from "dotenv";
-import { configureSocket } from "./socket.js";
+import express from 'express'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import { configureSocket } from './socket.js'
 
-dotenv.config();
+dotenv.config()
 
 // Create an Express app
-const app = express();
+const app = express()
 
 // Define the allowed origin
-const allowedOrigin = 'https://realtime-code-mentor-1.onrender.com';
+const allowedOrigin = 'https://realtime-code-mentor-1.onrender.com'
+
+// const allowedOrigin = "http://localhost:5173";
 
 // Enable CORS middleware for Express with specific origin
-app.use(cors({
-  origin: allowedOrigin,
-  methods: ['GET', 'POST'],  // You can add more methods if needed
-  credentials: true          // If credentials are needed
-}));
+app.use(
+  cors({
+    origin: allowedOrigin,
+    methods: ['GET', 'POST'], // You can add more methods if needed
+    credentials: true // If credentials are needed
+  })
+)
 
 // Get the port number from environment variables
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 
 // Create an HTTP server using Express app
-const httpServer = createServer(app);
+const httpServer = createServer(app)
 
 // Create a new instance of Socket.IO server with specific CORS settings
 const io = new Server(httpServer, {
   cors: {
     origin: allowedOrigin, // Match the allowed origin
-    methods: ["GET", "POST"], // Match the methods
+    methods: ['GET', 'POST'], // Match the methods
     credentials: true // Necessary if cookies or authentication are used
-  },
-});
+  }
+})
 
 // Configure Socket.IO server
-configureSocket(io);
+configureSocket(io)
 
 // Start the HTTP server and listen on the specified port
 httpServer.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
--- backend/socket.js
+++ backend/socket.js
@@ -1,105 +1,104 @@
 const configureSocket = (io) => {
   // Initialize an object to store information about code block sessions
-  const codeBlockSessions = {};
+  const codeBlockSessions = {}
 
   // Listen for connection events
-  io.on("connection", (socket) => {
-    console.log("A user connected");
+  io.on('connection', (socket) => {
+    console.log('A user connected')
 
     // Handle joining a code block session
-    socket.on("join_code_block", (codeBlockId) => {
+    socket.on('join_code_block', (codeBlockId) => {
       if (!codeBlockSessions[codeBlockId]) {
         // If the code block session doesn't exist, create a new session
         codeBlockSessions[codeBlockId] = {
           mentor: socket.id, // Set the mentor's socket ID
           students: new Set(), // Initialize an empty set for students
-          code: "", // Initialize the code with an empty string
-          connectedClients: new Set([socket.id]), // Initialize set with the current socket ID
-        };
+          code: '', // Initialize the code with an empty string
+          connectedClients: new Set([socket.id]) // Initialize set with the current socket ID
+        }
         // Emit role assigned as mentor and send initial code to the mentor
-        socket.emit("role_assigned", "mentor");
-        socket.emit("initial_code", codeBlockSessions[codeBlockId].code);
+        socket.emit('role_assigned', 'mentor')
+        socket.emit('initial_code', codeBlockSessions[codeBlockId].code)
       } else {
         // If the code block session exists, add the client as a student
-        const session = codeBlockSessions[codeBlockId];
+        const session = codeBlockSessions[codeBlockId]
         if (!session.connectedClients.has(socket.id)) {
           // Check if the client has already joined
-          session.students.add(socket.id);
-          session.connectedClients.add(socket.id);
+          session.students.add(socket.id)
+          session.connectedClients.add(socket.id)
 
           // Emit role assigned as student and send initial code to the student
-          socket.emit("role_assigned", "student");
-          socket.emit("initial_code", session.code);
+          socket.emit('role_assigned', 'student')
+          socket.emit('initial_code', session.code)
           socket.emit(
-            "on_block",
+            'on_block',
             codeBlockSessions[codeBlockId].connectedClients.size
-          );
+          )
         } else {
           // Emit role assigned as mentor and send initial code to the student
-          socket.emit("role_assigned", "mentor");
-          socket.emit("initial_code", session.code);
+          socket.emit('role_assigned', 'mentor')
+          socket.emit('initial_code', session.code)
         }
       }
       // Join the socket to the code block room
-      socket.join(codeBlockId);
-    });
+      socket.join(codeBlockId)
+    })
 
     // Handle code update events
-    socket.on("code_update", (data) => {
-      const { codeBlockId, newCode } = data;
-      const session = codeBlockSessions[codeBlockId];
+    socket.on('code_update', (data) => {
+      const { codeBlockId, newCode } = data
+      const session = codeBlockSessions[codeBlockId]
       if (session) {
         // Update the code for the corresponding code block session
-        session.code = newCode;
+        session.code = newCode
         // Emit the new code to all clients in the session except the emitting client
-        socket.to(codeBlockId).emit("code_update", newCode);
+        socket.to(codeBlockId).emit('code_update', newCode)
       }
-    });
+    })
 
     // Add a new event listener for user leaving the page
-    socket.on("leaving_page", (codeBlockId) => {
-      console.log("A user left");
-      handleUserLeaving(socket, codeBlockId);
-    });
+    socket.on('leaving_page', (codeBlockId) => {
+      console.log('A user left')
+      handleUserLeaving(socket, codeBlockId)
+    })
 
     // Modify your disconnect handler
-    socket.on("disconnect", () => {
-      console.log("A user disconnected");
-      handleUserLeaving(socket);
-    });
-  });
+    socket.on('disconnect', () => {
+      console.log('A user disconnected')
+      handleUserLeaving(socket)
+    })
+  })
 
   // Function to handle user leaving (either by navigating away or disconnecting)
-  function handleUserLeaving(socket, codeBlockId = null) {
+  function handleUserLeaving (socket, codeBlockId = null) {
     if (codeBlockId) {
       // If codeBlockId is provided, only clean up that specific session
-      const session = codeBlockSessions[codeBlockId];
+      const session = codeBlockSessions[codeBlockId]
       if (session) {
         if (session.mentor === socket.id) {
-          session.mentor = -1;
+          session.mentor = -1
         } else {
-          session.students.delete(socket.id);
-          session.connectedClients.delete(socket.id);
+          session.students.delete(socket.id)
+          session.connectedClients.delete(socket.id)
         }
-        if (session.connectedClients.size === 0 && session.mentor === -1)
-          delete codeBlockSessions[codeBlockId];
+        if (session.connectedClients.size === 0 && session.mentor === -1) { delete codeBlockSessions[codeBlockId] }
       }
     } else {
       // If no codeBlockId is provided, clean up all sessions for this socket
       Object.keys(codeBlockSessions).forEach((id) => {
-        const session = codeBlockSessions[id];
+        const session = codeBlockSessions[id]
         if (session.mentor === socket.id) {
           // Notify other users in the session
-          socket.to(id).emit("mentor_left", socket.id);
+          socket.to(id).emit('mentor_left', socket.id)
         } else {
-          session.students.delete(socket.id);
+          session.students.delete(socket.id)
         }
 
-        session.connectedClients.delete(socket.id);
-        if (session.connectedClients.size === 0) delete codeBlockSessions[id];
-      });
+        session.connectedClients.delete(socket.id)
+        if (session.connectedClients.size === 0) delete codeBlockSessions[id]
+      })
     }
   }
-};
+}
 
-export { configureSocket };
+export { configureSocket }
